dbabbd4dcf0ceef1b7281832e0bd96af010a0d99
Rafa Gómez <rgomezcasas@gmail.com>
2024-01-22 11:24:18 +0100

    feat(4-1): allow to declare multiples queues at once

--- 04-generate_queues_automatically/1-create_queues_in_rabbitmq/src/app/scripts/configure-rabbitmq.ts
+++ 04-generate_queues_automatically/1-create_queues_in_rabbitmq/src/app/scripts/configure-rabbitmq.ts
@@ -3,15 +3,29 @@ import { RabbitMqConnection } from "../../contexts/shared/infrastructure/event_b
 const connection = new RabbitMqConnection();
 
 const exchangeName = "domain_events";
-const queueName = "codely.retention.send_welcome_email_on_user_registered";
-const bindingKey = "codely.shop.user.registered";
+
+const queues: {
+	name: string;
+	bindingKey: string;
+}[] = [
+	{
+		name: "codely.retention.send_welcome_email_on_user_registered",
+		bindingKey: "codely.shop.user.registered",
+	},
+	{
+		name: "codely.retention.update_last_activity_date_on_user_updated",
+		bindingKey: "codely.shop.user.*",
+	},
+];
 
 async function main(): Promise<void> {
 	await connection.connect();
 
 	await connection.declareExchange(exchangeName);
 
-	await connection.declareQueue(queueName, exchangeName, bindingKey);
+	await Promise.all(
+		queues.map((queue) => connection.declareQueue(queue.name, exchangeName, queue.bindingKey)),
+	);
 
 	await connection.close();
 }
